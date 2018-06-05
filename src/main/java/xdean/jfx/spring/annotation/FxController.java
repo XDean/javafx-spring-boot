26941bc6c210deca9f1090b2b5605a44f943f349
XDean <373216024@qq.com>
2018-06-05 08:43:20 +0800

    refactor FxControllerProcessor

--- src/main/java/xdean/jfx/spring/annotation/FxController.java
+++ src/main/java/xdean/jfx/spring/annotation/FxController.java
@@ -4,9 +4,11 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+@Inherited
 @Documented
 @Retention(RUNTIME)
 @Target(TYPE)
--- src/main/java/xdean/jfx/spring/starter/FxControllerProcessor.java
+++ src/main/java/xdean/jfx/spring/starter/FxControllerProcessor.java
@@ -4,6 +4,8 @@ import static xdean.jex.util.lang.ExceptionUtil.uncheck;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.WeakHashMap;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanCreationException;
@@ -22,27 +24,37 @@ import xdean.jfx.spring.annotation.FxReady;
 @FxReady
 public class FxControllerProcessor implements InstantiationAwareBeanPostProcessor {
   private final Method getRoot = uncheck(() -> FxGetRoot.class.getMethod("getRoot"));
+  private final Map<Object, Object> controllerToRoot = new WeakHashMap<>();
 
   @Override
-  public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+  public boolean postProcessAfterInstantiation(Object controller, String beanName) throws BeansException {
+    Class<? extends Object> beanClass = controller.getClass();
     FxController fxController = AnnotationUtils.getAnnotation(beanClass, FxController.class);
     if (fxController == null) {
-      return null;
+      return true;
     }
     FXMLLoader fxmlLoader = new FXMLLoader(beanClass.getResource(fxController.fxml()));
+    fxmlLoader.setControllerFactory(c -> controller);
     try {
       fxmlLoader.load();
     } catch (IOException e) {
       throw new BeanCreationException("Fail to load fxml: " + fxmlLoader.getLocation(), e);
     }
-    Object controller = fxmlLoader.getController();
     Object root = fxmlLoader.getRoot();
+    controllerToRoot.put(controller, root);
+    return true;
+  }
+
+  @Override
+  public Object postProcessBeforeInitialization(Object controller, String beanName) throws BeansException {
+    Class<? extends Object> beanClass = controller.getClass();
     if (controller instanceof FxGetRoot) {
+      Object root = controllerToRoot.remove(controller);
       return Enhancer.create(beanClass, (MethodInterceptor) (obj, method, args, proxy) -> {
         if (method.equals(getRoot)) {
           return root;
         }
-        return proxy.invokeSuper(obj, args);
+        return method.invoke(controller, args);
       });
     } else {
       return controller;
