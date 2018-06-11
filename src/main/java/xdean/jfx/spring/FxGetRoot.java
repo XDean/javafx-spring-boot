1e3dcbace396900e30a3772c4859ae7160a3602f
XDean <373216024@qq.com>
2018-06-11 16:43:22 +0800

    remove fxGetRoot

--- src/main/java/xdean/jfx/spring/FxGetRoot.java
+++
@@ -1,9 +0,0 @@
-package xdean.jfx.spring;
-
-import xdean.jfx.spring.processor.FxControllerProcessor;
-
-public interface FxGetRoot<T> {
-  default T getRoot() {
-    return FxControllerProcessor.getRoot(this);
-  }
-}
--- src/main/java/xdean/jfx/spring/processor/FxControllerProcessor.java
+++ src/main/java/xdean/jfx/spring/processor/FxControllerProcessor.java
@@ -1,38 +1,54 @@
 package xdean.jfx.spring.processor;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.WeakHashMap;
+
+import javax.inject.Inject;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Component;
 
 import javafx.fxml.FXMLLoader;
 import xdean.jex.log.Logable;
-import xdean.jfx.spring.FxGetRoot;
+import xdean.jfx.spring.FxInitializable;
 import xdean.jfx.spring.annotation.FxController;
 import xdean.jfx.spring.annotation.FxReady;
+import xdean.jfx.spring.annotation.FxThread;
+import xdean.jfx.spring.annotation.ThreadScope;
 
+@ThreadScope
 @Component
 @FxReady
-public class FxControllerProcessor implements InstantiationAwareBeanPostProcessor, BeanFactoryAware, Logable {
-  private static final Map<Object, Object> CONTROLLER_TO_ROOT = new WeakHashMap<>();
+public class FxControllerProcessor
+    implements  InstantiationAwareBeanPostProcessor, BeanFactoryAware, Logable {
+  @Inject
+  @Qualifier(FxThread.SCHEDULER)
+  TaskExecutor scheduler;
 
   private BeanFactory beanFactory;
 
+  private boolean fxmling;
+
   @Override
   public boolean postProcessAfterInstantiation(Object controller, String beanName) throws BeansException {
+    // If fxmling, it means this controller is a sub-controller, it should be inject by
+    // parent-fxml-loader.
     Class<? extends Object> beanClass = controller.getClass();
     FxController fxController = AnnotationUtils.getAnnotation(beanClass, FxController.class);
     if (fxController == null) {
       return true;
     }
+    if (fxmling) {
+      return true;
+    }
     debug("Load fxml, class:{}, source:{}.", beanClass, fxController.fxml());
+    fxmling = true;
     FXMLLoader fxmlLoader = new FXMLLoader(beanClass.getResource(fxController.fxml()));
     fxmlLoader.setControllerFactory(c -> {
       if (c == beanClass) {
@@ -46,14 +62,15 @@ public class FxControllerProcessor implements InstantiationAwareBeanPostProcesso
     } catch (IOException e) {
       throw new BeanCreationException("Fail to load fxml: " + fxmlLoader.getLocation(), e);
     }
-    Object root = fxmlLoader.getRoot();
-    CONTROLLER_TO_ROOT.put(controller, root);
     return true;
   }
 
-  @SuppressWarnings("unchecked")
-  public static <T> T getRoot(FxGetRoot<T> controller) {
-    return (T) CONTROLLER_TO_ROOT.get(controller);
+  @Override
+  public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+    if (bean instanceof FxInitializable) {
+      scheduler.execute(() -> ((FxInitializable) bean).initAfterFxSpringReady());
+    }
+    return bean;
   }
 
   @Override
