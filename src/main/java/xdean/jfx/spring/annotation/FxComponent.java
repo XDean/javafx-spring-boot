4bd2f7a5c871fdaaa7d45d62017bfad3e36126fc
XDean <373216024@qq.com>
2018-06-25 19:07:25 +0800

    let fxComponent default singleton

--- src/main/java/xdean/jfx/spring/annotation/FxComponent.java
+++ src/main/java/xdean/jfx/spring/annotation/FxComponent.java
@@ -7,21 +7,18 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Component;
 
 /**
- * Indicate the class is a javafx component. JavaFx component default has prototype scope.
- * 
+ * Indicate the class is a javafx component
+ *
  * @author Dean Xu (XDean@github.com)
  */
 @Documented
 @Retention(RUNTIME)
 @Target(TYPE)
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public @interface FxComponent {
 
   @AliasFor(annotation = Component.class)
