fc9f5052210dd0515da45f25db9f5f038fa72a86
XDean <373216024@qq.com>
2018-07-01 20:31:22 +0800

    mark

--- src/main/java/xdean/jfx/spring/splash/FxSplash.java
+++ src/main/java/xdean/jfx/spring/splash/FxSplash.java
@@ -2,14 +2,34 @@ package xdean.jfx.spring.splash;
 
 import org.springframework.boot.context.event.ApplicationStartingEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.core.annotation.AnnotationUtils;
 
+import xdean.jex.log.Logable;
+import xdean.jfx.spring.annotation.Splash;
+import xdean.jfx.spring.context.FxContext;
 import xdean.spring.auto.AutoSpringFactories;
 
 @AutoSpringFactories(ApplicationListener.class)
-public class FxSplash implements ApplicationListener<ApplicationStartingEvent> {
+public class FxSplash implements ApplicationListener<ApplicationStartingEvent>, Logable {
 
   @Override
   public void onApplicationEvent(ApplicationStartingEvent event) {
-    System.out.println(event.getSpringApplication().getMainApplicationClass());
+    Class<?> mainClass = event.getSpringApplication().getMainApplicationClass();
+    Splash splash = AnnotationUtils.getAnnotation(mainClass, Splash.class);
+    if (splash == null) {
+      debug("Not find @Splash, try find system properties.");
+      if (!Boolean.getBoolean(Splash.SPLASH)) {
+        debug("Not open splash, skip it.");
+        return;
+      }
+    }
+    debug("Splash enabled. Start fx context.");
+    String[] args = System.getProperties().getProperty(FxContext.FX_ARGS, "").split(" ");
+    try {
+      FxContext.start(args);
+    } catch (InterruptedException e) {
+      warn("Start fx context interrupted.");
+      System.exit(1);
+    }
   }
 }
