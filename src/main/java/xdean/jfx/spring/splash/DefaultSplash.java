d33640a9db5a1c5f82aff829725f5768af52ca87
XDean <373216024@qq.com>
2018-07-02 16:46:03 +0800

    handle preload progress

--- src/main/java/xdean/jfx/spring/splash/DefaultSplash.java
+++ src/main/java/xdean/jfx/spring/splash/DefaultSplash.java
@@ -1,19 +1,28 @@
 package xdean.jfx.spring.splash;
 
+import static xdean.jfxex.bean.ListenerUtil.on;
+
 import java.io.IOException;
 
+import org.springframework.util.Assert;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
+import xdean.jex.log.Logable;
 import xdean.jfxex.support.DragSupport;
-import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.control.ProgressBar;
 
-public class DefaultSplash implements SplashService, PreloadReporter {
+public class DefaultSplash implements SplashService, PreloadReporter, Logable {
 
   private @FXML AnchorPane root;
   private @FXML AnchorPane container;
@@ -23,9 +32,15 @@ public class DefaultSplash implements SplashService, PreloadReporter {
   private @FXML Label minorProgressLabel;
   private @FXML ProgressBar progressBar;
 
+  private Stage primaryStage;
+  private Stage splashStage;
+  private boolean done;
+
   @Override
   public void createSplash(Stage primaryStage) {
-    Stage stage = new Stage(StageStyle.UNDECORATED);
+    debug("Create default splash");
+    this.primaryStage = primaryStage;
+    splashStage = new Stage(StageStyle.TRANSPARENT);
 
     FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/DefaultSplash.fxml"));
     loader.setController(this);
@@ -35,23 +50,69 @@ public class DefaultSplash implements SplashService, PreloadReporter {
       throw new Error("Load default splash fxml fail.", e);
     }
 
-    stage.setScene(new Scene(root));
+    splashStage.setScene(new Scene(root));
 
-    stage.setAlwaysOnTop(true);
-    stage.setWidth(700);
-    stage.setHeight(400);
-    stage.show();
+    splashStage.setAlwaysOnTop(true);
+    splashStage.setWidth(700);
+    splashStage.setHeight(400);
+    splashStage.show();
 
-    DragSupport.bind(stage);
+    DragSupport.bind(splashStage);
+
+    primaryStage.showingProperty().addListener(on(true, () -> done()));
   }
 
   @Override
   public SubReporter load(String title) {
-    return null;
+    return new SubReporter() {
+      int count = -1;
+      int task;
+
+      @Override
+      public void setCount(int count) {
+        Assert.isTrue(this.count == -1, "Can't set count twice");
+        this.count = count;
+      }
+
+      @Override
+      public void load(String subTitle) {
+        run(() -> {
+          this.task++;
+          majorTaskLabel.setText(title);
+          minorTaskLabel.setText(subTitle);
+          if (count >= task) {
+            minorProgressLabel.setText(String.format("(%d/%d)", task, count));
+          }
+        });
+      }
+    };
   }
 
   @Override
   public void done() {
+    if (!done) {
+      run(() -> {
+        if (!done) {
+          done = true;
+          debug("Close splash stage");
+          Timeline timeline = new Timeline();
+          timeline.getKeyFrames().add(new KeyFrame(Duration.millis(1200),
+              new KeyValue(splashStage.getScene().getRoot().opacityProperty(), 0.3)));
+          timeline.setOnFinished(e -> {
+            primaryStage.show();
+            splashStage.close();
+          });
+          timeline.play();
+        }
+      });
+    }
+  }
 
+  private void run(Runnable task) {
+    if (Platform.isFxApplicationThread()) {
+      task.run();
+    } else {
+      Platform.runLater(task);
+    }
   }
 }
--- src/test/java/xdean/jfx/spring/sample/SampleApp.java
+++ src/test/java/xdean/jfx/spring/sample/SampleApp.java
@@ -1,5 +1,6 @@
 package xdean.jfx.spring.sample;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.springframework.boot.SpringApplication;
@@ -12,6 +13,8 @@ import xdean.jfx.spring.FxmlResult;
 import xdean.jfx.spring.annotation.Splash;
 import xdean.jfx.spring.annotation.SpringFxApplication;
 import xdean.jfx.spring.splash.FxSplash;
+import xdean.jfx.spring.splash.PreloadReporter;
+import xdean.jfx.spring.splash.PreloadReporter.SubReporter;
 
 @Splash
 @SpringFxApplication
@@ -22,15 +25,30 @@ public class SampleApp implements FxApplication {
     app.run(args);
   }
 
-  @Inject
-  FxmlResult<SampleController, Parent> controller;
+  private @Inject PreloadReporter preloadReporter;
+
+  private @Inject FxmlResult<SampleController, Parent> controller;
 
   @Override
   public void start(Stage stage) throws Exception {
     Scene scene = new Scene(controller.getRoot(), 400, 300);
-
     stage.setTitle("Hello JavaFX-Spring-Boot");
     stage.setScene(scene);
     stage.show();
+  }
+
+  @PostConstruct
+  public void mockPreload() throws InterruptedException {
+    int task = (int) (3 + 3 * Math.random());
+    for (int i = 0; i < task; i++) {
+      SubReporter subReporter = preloadReporter.load("Preload task " + i);
+      int subTask = (int) (3 + 3 * Math.random());
+      subReporter.setCount(subTask);
+      for (int s = 0; s < subTask; s++) {
+        subReporter.load("Sub preload task " + i + "." + s);
+        Thread.sleep((long) (Math.random() * 300 + 100));
+      }
+    }
+    preloadReporter.done();
   }
 }
